118c66fd0950c210ea915de2b9d6db7c0000e281
nirvik <nirvik@Jarvis>
2022-04-29 08:01:54 +0000

    Add project files.

---
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //registerign logger with Dependency injection
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //up
+
+
+        //these are two action method for router {action}
+
+        public IActionResult Index() //IActionResult can return multiple return type
+        {
+            return View(); 
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //up
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
